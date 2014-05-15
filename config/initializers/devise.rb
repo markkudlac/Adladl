5e7428e47b497b0bf888f6757ae268b43ce25aae
Mark Kudlac <mark.kudlac@sympatico.ca>
2014-05-15 16:44:41 -0400

    mailgun spam is problem

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,8 +10,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'postmaster@app24858184.mailgun.org'
-
+  # config.mailer_sender = 'postmaster@app24858184.mailgun.org'
+  config.mailer_sender = 'noreply@adladl.com'
+  
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
